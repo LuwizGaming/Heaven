e674e8a076b534de3491c7277b0a1cec0fc54981
LuwizGaming <65932190+LuwizGaming@users.noreply.github.com>
2026-05-03 18:22:51 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const items = [
+  {
+    title: "Item One",
+    image: "assets/img/background.jpg"
+  },
+  {
+    title: "Item Two",
+    image: "assets/img/background.jpg"
+  },
+  {
+    title: "Item Three",
+    image: "assets/img/background.jpg"
+  }
+];
